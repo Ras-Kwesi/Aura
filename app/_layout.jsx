0bb1ea17abf4e6c08c359c732bdce5c620f4a5b9
kwesi <kwesi.makonnen@gmail.com>
2024-06-27 00:00:43 +0300

    App Natural Setup up

---
+++ app/_layout.jsx
@@ -0,0 +1,8 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router'
+
+const RootLayout = () => {
+  return <Slot />
+}
+export default RootLayout
