87e1cda962b6daa2255fc9452e8563303fc56a47
kwesi <kwesi.makonnen@gmail.com>
2024-06-27 00:20:40 +0300

    Stack used for routing, Native wind added

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,15 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Slot, Stack } from 'expo-router'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Slot, Stack } from "expo-router";
 
 const RootLayout = () => {
   return (
-    <>
-    <Text>Header</Text>
-  <Slot />  
-  {/* Returns the current child route */}
-    <Text>Footer</Text>
-    </>
-  )
-}
-export default RootLayout
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false, title: "Home" }}
+      />
+    </Stack>
+  );
+};
+export default RootLayout;
