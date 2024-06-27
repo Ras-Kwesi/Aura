9d75ba1a7a321f9a4e6d3502c1fc70a01d35d6f7
kwesi <kwesi.makonnen@gmail.com>
2024-06-27 18:05:26 +0300

    Layout Routing set up

--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack ">Aura!</Text>
       <StatusBar style="auto" />
-      <Link className='text-blue-500 font-plight' href="/profile">Go to Profile</Link>
+      <Link className='text-blue-500 font-plight' href="/home">Go to Home</Link>
     </View>
   );
 }
