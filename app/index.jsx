95f8f427fe832e6b93c4fe60911cd73259804539
kwesi <kwesi.makonnen@gmail.com>
2024-06-27 19:25:50 +0300

    Home Page Set up

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { Link, Redirect, router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {images} from '../constants'
+import CustomButton from '../components/CustomButton';
+
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack ">Aura!</Text>
-      <StatusBar style="auto" />
-      <Link className='text-blue-500 font-plight' href="/home">Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+        <ScrollView contentContainerStyle={{height: '100%'}}>
+            <View className="w-full items-center justify-center min-h-[85vh] px-4">
+                <Image
+                    source={images.logo}
+                    className='w-[130px] h-[84px]'
+                    resizeMode='contain'
+                />
+
+                <Image 
+                    source = {images.cards}
+                    className = 'max-w-[380px] w-full h-[300px]'
+                    resizeMode='contain'
+                />
+
+                <View className='relative mt-5'>
+                    <Text className='text-3xl text-white font-bold text-center'>
+                        Discover New Infinite Worlds with <Text className='text-secondary-200'>Aura</Text>
+                    </Text>
+                    <Image 
+                        source={images.path}
+                        className='w=[136px] h-[15px] absolute -bottom-2 right-12'
+                        resizeMode='contain'
+                    />
+                </View>
+                <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>
+                    Where Creativity Meets Innovation: Embark on a Journey of Limitless Exploration with Aora
+                </Text>
+
+                <CustomButton
+                    title='Continue with Email'
+                    handlePress = {() => {router.push('/sign-in')}}
+                    containerStyles = 'w-full mt-12'
+                />
+
+                <StatusBar 
+                    backgroundColor='#161622'
+                    style='light'                
+                />
+            </View>
+        </ScrollView>
+    </SafeAreaView>
   );
 }
