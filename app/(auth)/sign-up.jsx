675d4dcc68b0fc6a1d52e035f4297a2f864b143d
kwesi <kwesi.makonnen@gmail.com>
2024-06-30 17:58:40 +0300

    Authentication works

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,10 +1,11 @@
-import { View, Text, ScrollView, Image } from 'react-native';
+import { View, Text, ScrollView, Image, Alert } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
+import {createUser} from '../../lib/appwrite'
 
 
 const SignUp = () => {
@@ -15,8 +16,24 @@ const SignUp = () => {
         password: ''
     })
 
-    const submit = () => {
-        console.log(form)
+    const submit = async () => {
+        console.log('Submit event started')
+        if (!form.username || !form.email || !form.password) {
+            Alert.alert('Error', 'Please fill in all fields');
+            
+            // Set to global state using react context
+
+        }
+        setIsSubmitting(true);
+        try {
+            const result = await createUser(form.email, form.password, form.username);
+            console.log('result\n\n',result,'\n\n')
+            router.replace('/home')
+        } catch (error) {
+            Alert.alert('Error', error.message); 
+        } finally {
+            setIsSubmitting(false);
+        }
     }
 
     const [isSubmitting, setIsSubmitting] = useState(false)
@@ -67,10 +84,10 @@ const SignUp = () => {
                 />
                 <View className='justify-center pt-5 flex-row gap-2'>
                     <Text className='text-lg text-gray-100 font-pregular'>
-                        Already ave an account?
+                        Already have an account?
                     </Text>
                     <Link href='/sign-in' className='text-lg text-secondary font-psemibold'>
-                        Sign in!
+                        Sign in
                     </Link>
 
                 </View>
--- app/index.jsx
+++ app/index.jsx
@@ -4,6 +4,7 @@ import { Link, Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {images} from '../constants'
 import CustomButton from '../components/CustomButton';
+// import 'react-native-url-polyfill/auto'
 
 
 export default function App() {
