9440f06271b26bff9880c6acb9b7e619f114728c
kwesi <kwesi.makonnen@gmail.com>
2024-08-19 14:39:11 +0300

    changed to readme

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -1,11 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import VideoCard from '../../components/VideoCard'
 
 const Bookmark = () => {
   return (
-    <View>
-      <Text>Bookmark</Text>
-    </View>
+    <SafeAreaView className = 'bg-primary h-full'>
+        <FlatList 
+            data = '{likes}'
+            keyExtractor={(item) => item.$id}
+            renderItem = {({item}) => (
+                <VideoCard 
+                    
+                />
+            )}
+        />
+    </SafeAreaView>
   )
 }
 
